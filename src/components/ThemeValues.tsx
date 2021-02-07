06ddfd94fc21b509135a54c172829ca6fc881260
tcuevas92 <tcuevas92@gmail.com>
2021-02-07 14:50:14 -0500

    Code cleanup and more basic styles

--- src/components/ThemeValues.tsx
+++ src/components/ThemeValues.tsx
@@ -3,37 +3,44 @@ import './ThemeValues.css';
 import Input from '../core_ui/Input/Input';
 
 const getCustomProperties = (styleSheet: StyleSheet): any[] => {
-	return [...(styleSheet as any).cssRules].filter(rule => rule.style != null).reduce((properties, rule) => {
-		const props = [...rule.style].map(propName => {
-			return [propName.trim(), rule.style.getPropertyValue(propName).trim()]
-		})
-		.filter(p => p[0].startsWith('--'));
+	return [...(styleSheet as any).cssRules]
+		.filter((rule) => rule.style != null)
+		.reduce((properties, rule) => {
+			const props = [...rule.style]
+				.map((propName) => {
+					return [
+						propName.trim(),
+						rule.style.getPropertyValue(propName).trim(),
+					];
+				})
+				.filter((p) => p[0].startsWith('--'));
 
-		return [...properties, ...props];
-	}, []);
-}
+			return [...properties, ...props];
+		}, []);
+};
 
 const ThemeValues = () => {
 	const [cssCustomProperties, setCssCustomProperties] = useState<any[]>([]);
 	useEffect(() => {
 		const styleSheets = [...document.styleSheets];
-		const customProperties = styleSheets.reduce<any[]>((allProperties, styleSheet) => [...allProperties, ...getCustomProperties(styleSheet)], []);
+		const customProperties = styleSheets.reduce<any[]>(
+			(allProperties, styleSheet) => [
+				...allProperties,
+				...getCustomProperties(styleSheet),
+			],
+			[],
+		);
 
 		setCssCustomProperties(customProperties);
 	}, []);
 
 	return (
 		<div className="values-display">
-			{
-				cssCustomProperties.map((p, i) => (
-					<div key={i}>
-						<CustomPropertyInput
-							name={p[0]}
-							initialValue={p[1]}
-						/>
-					</div>
-				))
-			}
+			{cssCustomProperties.map((p, i) => (
+				<div key={i}>
+					<CustomPropertyInput name={p[0]} initialValue={p[1]} />
+				</div>
+			))}
 		</div>
 	);
 };
@@ -43,20 +50,26 @@ interface ICustomPropertyInputProps {
 	initialValue: string;
 }
 
-const CustomPropertyInput = ({ name, initialValue }: ICustomPropertyInputProps) => {
+const CustomPropertyInput = ({
+	name,
+	initialValue,
+}: ICustomPropertyInputProps) => {
 	const [value, setValue] = useState(initialValue);
 
 	return (
 		<div>
 			<label>{name}</label>
 			<Input
-				type="text"
+				type={initialValue.startsWith('#') ? 'color' : 'text'}
 				name={name}
 				value={value}
 				onChange={(event) => {
 					const rootElement = document.querySelector(':root');
 					if (rootElement) {
-						(rootElement as any).style.setProperty(event.target.name, event.target.value);
+						(rootElement as any).style.setProperty(
+							event.target.name,
+							event.target.value,
+						);
 					}
 
 					setValue(event.target.value);
@@ -64,5 +77,5 @@ const CustomPropertyInput = ({ name, initialValue }: ICustomPropertyInputProps) 
 			/>
 		</div>
 	);
-}
+};
 export default ThemeValues;
--- src/pages/KitchenSink.tsx
+++ src/pages/KitchenSink.tsx
@@ -6,60 +6,110 @@ import ThemeValues from '../components/ThemeValues';
 interface KitchenSinkProps {}
 
 function KitchenSink({}: KitchenSinkProps) {
-  return (
-    <>
-    <main>
-      <h1>A Visual Type Scale</h1>
+	return (
+		<>
+			<header>
+				<h3>Bleh</h3>
+				<nav>
+					<a href="#">Home</a>
+					<a href="#">About</a>
+					<a href="#">Contact</a>
+				</nav>
+			</header>
+			<main>
+				<h1>A Visual Type Scale</h1>
 
-<p>What looked like a small patch of purple grass, above five feet square, was moving across the sand in their direction.</p>
+				<p>
+					What looked like a <a href="#">small patch</a> of purple grass, above
+					five feet square, was moving across the sand in their direction.
+				</p>
 
-<p>When it came near enough he perceived that it was not grass; there were no blades, but only purple roots. The roots were revolving, for each small plant in the whole patch, like the spokes of a rimless wheel.</p>
+				<p>
+					When it came near enough he perceived that it was not grass; there
+					were no blades, but only purple roots. The roots were revolving, for
+					each small plant in the whole patch, like the spokes of a rimless
+					wheel.
+				</p>
 
-<h2>A Visual Type Scale</h2>
+				<h2>A Visual Type Scale</h2>
 
-<p>What looked like a small patch of purple grass, above five feet square, was moving across the sand in their direction.</p>
+				<p>
+					What looked like a small patch of purple grass, above five feet
+					square, was moving across the sand in their direction.
+				</p>
 
-<p>When it came near enough he perceived that it was not grass; there were no blades, but only purple roots. The roots were revolving, for each small plant in the whole patch, like the spokes of a rimless wheel.</p>
+				<p>
+					When it came near enough he perceived that it was not grass; there
+					were no blades, but only purple roots. The roots were revolving, for
+					each small plant in the whole patch, like the spokes of a rimless
+					wheel.
+				</p>
 
-<Blockquote>
-There are many <strong>variations of passages</strong> of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. 
-</Blockquote>
-<h3>A Visual Type Scale</h3>
+				<Blockquote>
+					There are many <strong>variations of passages</strong> of Lorem Ipsum
+					available, but the majority have suffered alteration in some form, by
+					injected <a href="#">humour</a>, or randomised words which don't look
+					even slightly believable.
+				</Blockquote>
+				<h3>A Visual Type Scale</h3>
 
-<p>What looked like a small patch of purple grass, above five feet square, was moving across the sand in their direction.</p>
+				<p>
+					What looked like a small patch of purple grass, above five feet
+					square, was moving across the sand in their direction.
+				</p>
 
-<p>When it came near enough he perceived that it was not grass; there were no blades, but only purple roots. The roots were revolving, for each small plant in the whole patch, like the spokes of a rimless wheel.</p>
+				<p>
+					When it came near enough he perceived that it was not grass; there
+					were no blades, but only purple roots. The roots were revolving, for
+					each small plant in the whole patch, like the spokes of a rimless
+					wheel.
+				</p>
 
-<h4>A Visual Type Scale</h4>
+				<h4>A Visual Type Scale</h4>
 
-<p>What looked like a small patch of purple grass, above five feet square, was moving across the sand in their direction.</p>
+				<p>
+					What looked like a small patch of purple grass, above five feet
+					square, was moving across the sand in their direction.
+				</p>
 
-<p>When it came near enough he perceived that it was not grass; there were no blades, but only purple roots. The roots were revolving, for each small plant in the whole patch, like the spokes of a rimless wheel.</p>
+				<p>
+					When it came near enough he perceived that it was not grass; there
+					were no blades, but only purple roots. The roots were revolving, for
+					each small plant in the whole patch, like the spokes of a rimless
+					wheel.
+				</p>
 
-<ul>
-  <li>Hey</li>
-  <li>Hello</li>
-  <li>It's me</li>
-</ul>
+				<ul>
+					<li>Hey</li>
+					<li>Hello</li>
+					<li>It's me</li>
+				</ul>
 
-<h5>A Visual Type Scale</h5>
+				<h5>A Visual Type Scale</h5>
 
-<p>What looked like a small patch of purple grass, above five feet square, was moving across the sand in their direction.</p>
+				<p>
+					What looked like a small patch of purple grass, above five feet
+					square, was moving across the sand in their direction.
+				</p>
 
-<p>When it came near enough he perceived that it was not grass; there were no blades, but only purple roots. The roots were revolving, for each small plant in the whole patch, like the spokes of a rimless wheel.</p>
+				<p>
+					When it came near enough he perceived that it was not grass; there
+					were no blades, but only purple roots. The roots were revolving, for
+					each small plant in the whole patch, like the spokes of a rimless
+					wheel.
+				</p>
 
-<p className="text_small">— Excerpt from A Voyage to Arcturus, by David Lindsay.</p>
-<Input name="fo" type="text" />
-<Input name="fo" type="number" />
-<Input name="fo" type="date" />
-<Input name="fo" type="tel" />
-
-    </main>
-<ThemeValues />
-    </>
-  );
-
-
+				<p className="text_small">
+					— Excerpt from A Voyage to Arcturus, by David Lindsay.
+				</p>
+				<Input name="fo" type="text" />
+				<Input name="fo" type="number" />
+				<Input name="fo" type="date" />
+				<Input name="fo" type="tel" />
+			</main>
+			<ThemeValues />
+		</>
+	);
 }
 
 export default KitchenSink;
